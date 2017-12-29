881034c171dca21b28431f82e208790e4877220c
smiile8888 <th.thanyathorn@gmail.com>
2017-12-29 22:51:01 +0700

    Updated

--- js/app.js
+++ js/app.js
@@ -33,32 +33,25 @@ $(document).ready(function(){
     });
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Feb 1, 2018 00:00:00").getTime();
+    const countDownDate = new Date("Feb 1, 2018 00:00:00").getTime();
+    const timer = setInterval(function() {
+      const now = new Date().getTime();
 
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-      // Get todays date and time
-      var now = new Date().getTime();
-
-      // Find the distance between now an the count down date
-      var distance = countDownDate - now;
+      const distance = countDownDate - now;
 
       // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Display the result in an element with id="demo"
       document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
       + minutes + "m " + seconds + "s ";
 
       // If the count down is finished, write some text 
       if (distance < 0) {
-        clearInterval(x);
+        clearInterval(timer);
         document.getElementById("countdown-timer").innerHTML = "EXPIRED";
       }
     }, 1000);
-
   });
