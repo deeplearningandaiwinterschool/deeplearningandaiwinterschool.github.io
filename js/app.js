42c960b3a9ad59b1686596dfe386a808005cb998
smiile8888 <th.thanyathorn@gmail.com>
2018-01-10 23:29:41 +0700

    Added Google Analytics

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
-$(document).ready(function(){$(".navbar a, footer a[href='#myPage']").on('click', function(event){if (this.hash !==""){event.preventDefault(); var hash=this.hash; $('html, body').animate({scrollTop: $(hash).offset().top}, 500, function(){window.location.hash=hash;});}}); $(window).scroll(function(){$(".slideanim").each(function(){var pos=$(this).offset().top; var winTop=$(window).scrollTop(); if (pos < winTop + 600){$(this).addClass("slide");}});}); const countDownDate=new Date("Feb 1, 2018 00:00:00").getTime(); const timer=setInterval(function(){const now=new Date().getTime(); const distance=countDownDate - now; const days=Math.floor(distance / (1000 * 60 * 60 * 24)); const hours=Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); const minutes=Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); const seconds=Math.floor((distance % (1000 * 60)) / 1000); document.getElementById("countdown-timer").innerHTML=days + "d " + hours + "h " + minutes + "m " + seconds + "s "; if (distance < 0){clearInterval(timer); document.getElementById("countdown-timer").innerHTML="EXPIRED";}}, 1000);});
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    
+    $(window).scroll(function() {
+      $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+  
+        var winTop = $(window).scrollTop();
+          if (pos < winTop + 600) {
+            $(this).addClass("slide");
+          }
+      });
+    });
+
+    // Set the date we're counting down to
+    const countDownDate = new Date("Feb 1, 2018 00:00:00").getTime();
+    const timer = setInterval(function() {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(timer);
+        document.getElementById("countdown-timer").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+  });
