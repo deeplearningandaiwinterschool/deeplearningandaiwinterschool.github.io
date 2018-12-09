575118ff11b675834120ac036ecefc45b03eee28
Panissara <panissara@supsangsook.com>
2018-12-09 09:54:21 +0700

    Change countdown script to start on 06.00a.m.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-$(document).ready(function(){$(".navbar a, footer a[href='#myPage']").on('click',function(event){if(this.hash!==""){event.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(hash).offset().top},500,function(){window.location.hash=hash;});}});$(window).scroll(function(){$(".slideanim").each(function(){var pos=$(this).offset().top;var winTop=$(window).scrollTop();if(pos<winTop+600){$(this).addClass("slide");}});});const countDownDate=new Date("Dec 10, 2018 00:00:00").getTime();const timer=setInterval(function(){const now=new Date().getTime();const distance=countDownDate-now;const days=Math.floor(distance/(1000*60*60*24));const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));const minutes=Math.floor((distance%(1000*60*60))/(1000*60));const seconds=Math.floor((distance%(1000*60))/1000);document.getElementById("countdown-timer").innerHTML=days+"d "+hours+"h " +minutes+"m "+seconds+"s ";if(distance<0){clearInterval(timer);document.getElementById("countdown-timer").innerHTML="EXPIRED";}},1000);});
+$(document).ready(function() {
+  $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        { scrollTop: $(hash).offset().top },
+        500,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+  $(window).scroll(function() {
+    $(".slideanim").each(function() {
+      var pos = $(this).offset().top;
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+  const countDownDate = new Date("Dec 10, 2018 06:00:00").getTime();
+  const timer = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown-timer").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+      clearInterval(timer);
+      document.getElementById("countdown-timer").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+});
