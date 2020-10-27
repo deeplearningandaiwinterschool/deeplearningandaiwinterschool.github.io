fc7558543279e7f81e4bbb465852043217a0eaf5
Smile Thanapattheerakul <th.thanyathorn@gmail.com>
2020-10-27 01:50:42 -0400

    Update font and countdown units

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     document.getElementById("countdown-timer").innerHTML =
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
     if (distance < 0) {
       clearInterval(timer);
       document.getElementById("countdown-timer").innerHTML = "EVENT TIME!";
