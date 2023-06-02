7e92a9ffdb6a6db37952dddd79102ab40a299a49
smiile8888 <th.thanyathorn@gmail.com>
2023-06-02 19:37:09 -0400

    fix js

--- js/app.js
+++ js/app.js
@@ -21,21 +21,23 @@ $(document).ready(function () {
       }
     });
   });
-  const countDownDate = new Date("June 14 ,2022 06:00:00").getTime();
-  const timer = setInterval(function () {
-    const now = new Date().getTime();
-    const distance = countDownDate - now;
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("countdown-timer").innerHTML =
-      days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
-    if (distance < 0) {
-      clearInterval(timer);
-      document.getElementById("countdown-timer").innerHTML = "EVENT TIME!";
-    }
-  }, 1000);
+  // Countdown function
+  // Uncomment when used
+  // const countDownDate = new Date("June 14 ,2022 06:00:00").getTime();
+  // const timer = setInterval(function () {
+  //   const now = new Date().getTime();
+  //   const distance = countDownDate - now;
+  //   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   const hours = Math.floor(
+  //     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //   );
+  //   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //   document.getElementById("countdown-timer").innerHTML =
+  //     days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+  //   if (distance < 0) {
+  //     clearInterval(timer);
+  //     document.getElementById("countdown-timer").innerHTML = "EVENT TIME!";
+  //   }
+  // }, 1000);
 });
